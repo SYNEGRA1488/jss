278ef3ee508c32b3f3147781f1202d1872489cc6
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:02:40 +0200

    Update zadanie10_liev.js

--- zadanie10_liev.js
+++ zadanie10_liev.js
@@ -1,3 +1,6 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ var a=parseFloat(prompt('liczba1=','')); var b=parseFloat(prompt('liczba2=','')); document.getElementById('output_10').innerHTML = 'suma='+a+' + '+b+' = '+(a+b); })();
+(function(){ var a=parseFloat(prompt('liczba1=','')); 
+var b=parseFloat(prompt('liczba2=','')); 
+document.getElementById('output_10').innerHTML = 'suma='+a+' + '+b+' = '+(a+b); 
+})();
