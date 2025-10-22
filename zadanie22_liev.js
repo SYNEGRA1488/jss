3d54f58eb0ab7a3a6efbf34390b30878af1c38ae
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:14:44 +0200

    Refactor JavaScript functions for area and perimeter

--- zadanie22_liev.js
+++ zadanie22_liev.js
@@ -1,3 +1,10 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ document.getElementById('output_22').innerHTML = 'a: <input id="a_dana_lie" value="4"> b: <input id="b_dana_lie" value="5"><button onclick="Pole_lie()">Pole</button><button onclick="Obwod_lie()">Obwod</button><div id="wyprowadz_pole_lie"></div><div id="wyprowadz_obwod_lie"></div>'; window.Pole_lie=function(){var a=parseFloat(document.getElementById('a_dana_lie').value), b=parseFloat(document.getElementById('b_dana_lie').value); document.getElementById('wyprowadz_pole_lie').innerHTML='Pole='+ (a*b).toFixed(2);}; window.Obwod_lie=function(){var a=parseFloat(document.getElementById('a_dana_lie').value), b=parseFloat(document.getElementById('b_dana_lie').value); document.getElementById('wyprowadz_obwod_lie').innerHTML='Obwod='+ (2*(a+b)).toFixed(2);}; })();
+(function(){ document.getElementById('output_22').innerHTML = 'a: <input id="a_dana_lie" value="4"> b: <input id="b_dana_lie" value="5"><button onclick="Pole_lie()">Pole</button><button onclick="Obwod_lie()">Obwod</button><div id="wyprowadz_pole_lie"></div><div id="wyprowadz_obwod_lie"></div>'; 
+window.Pole_lie=function(){
+var a=parseFloat(document.getElementById('a_dana_lie').value), b=parseFloat(document.getElementById('b_dana_lie').value); 
+document.getElementById('wyprowadz_pole_lie').innerHTML='Pole='+ (a*b).toFixed(2);}; 
+window.Obwod_lie=function(){
+var a=parseFloat(document.getElementById('a_dana_lie').value), b=parseFloat(document.getElementById('b_dana_lie').value); 
+document.getElementById('wyprowadz_obwod_lie').innerHTML='Obwod='+ (2*(a+b)).toFixed(2);}; 
+})();
