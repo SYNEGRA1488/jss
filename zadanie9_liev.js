910d48e91a6f1ecbccb0bffc3ec9748685030bb7
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:17:09 +0200

    Refactor prompt box display function

--- zadanie9_liev.js
+++ zadanie9_liev.js
@@ -1,3 +1,7 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ document.getElementById('output_9').innerHTML='<button onclick="show_prompt()">Pokaż prompt box</button>'; window.show_prompt=function(){var name=prompt('Napisz imię','Kyrylo'); if(name!=null && name!='') document.getElementById('output_9').innerHTML='Witaj '+name+'! jak się masz?';}; })();
+(function(){ document.getElementById('output_9').innerHTML='<button onclick="show_prompt()">Pokaż prompt box</button>'; 
+window.show_prompt=function(){
+var name=prompt('Napisz imię','Kyrylo'); 
+if(name!=null && name!='') document.getElementById('output_9').innerHTML='Witaj '+name+'! jak się masz?';}; 
+})();
