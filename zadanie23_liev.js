be7661f1bb332baa7b54e2b6a8c49e73d5765bb3
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:15:16 +0200

    Refactor code for ladder length and angle calculation

--- zadanie23_liev.js
+++ zadanie23_liev.js
@@ -1,3 +1,8 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ var D=parseFloat(prompt('D - wysokość oparcia drabiny (m)','5')), X=parseFloat(prompt('X - odległość od muru (m)','3')); var length=Math.sqrt(D*D+X*X); var angleRad=Math.atan(D/X); var angleDeg=angleRad*180/Math.PI; document.getElementById('output_23').innerHTML='Długość drabiny = '+length.toFixed(2)+' m<br>Kąt nachylenia = '+angleDeg.toFixed(2)+' stopni'; })();
+(function(){ 
+var D=parseFloat(prompt('D - wysokość oparcia drabiny (m)','5')), X=parseFloat(prompt('X - odległość od muru (m)','3')); 
+var length=Math.sqrt(D*D+X*X); 
+var angleRad=Math.atan(D/X); 
+var angleDeg=angleRad*180/Math.PI; document.getElementById('output_23').innerHTML='Długość drabiny = '+length.toFixed(2)+' m<br>Kąt nachylenia = '+angleDeg.toFixed(2)+' stopni'; 
+})();
