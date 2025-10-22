e5c9398f3eb4e983a99c8555a2e3de2b9ebe2452
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:06:06 +0200

    Refactor code for better readability

--- zadanie14_liev.js
+++ zadanie14_liev.js
@@ -1,3 +1,8 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ var m=parseInt(prompt('m=','4')); var a=m+1; var b=m*(m+1); var c=b+1; var d=b*(b+1); document.getElementById('output_14').innerHTML = '1/'+m+' = 1/'+a+' + 1/'+c+' + 1/'+d; })();
+(function(){ var m=parseInt(prompt('m=','4')); 
+var a=m+1; 
+var b=m*(m+1); 
+var c=b+1; 
+var d=b*(b+1);
+document.getElementById('output_14').innerHTML = '1/'+m+' = 1/'+a+' + 1/'+c+' + 1/'+d; })();
