b07b0312a80658996bcb02075379d6cb709c5b47
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:03:25 +0200

    Update zadanie11_liev.js

--- zadanie11_liev.js
+++ zadanie11_liev.js
@@ -1,3 +1,7 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ var D1=parseInt(prompt('D1=','30')); var D2=parseInt(prompt('D2=','28')); var seconds=(D1-D2)*24*3600; document.getElementById('output_11').innerHTML = "<div style='color:green;font-size:" + (30+14) + "px;'>ilość sekund = " + seconds + " sekund</div>"; })();
+(function(){ var D1=parseInt(prompt('D1=','30')); 
+var D2=parseInt(prompt('D2=','28'));
+var seconds=(D1-D2)*24*3600; 
+document.getElementById('output_11').innerHTML = "<div style='color:green;font-size:" + (30+14) + "px;'>ilość sekund = " + seconds + " sekund</div>"; 
+})();
