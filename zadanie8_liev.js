28de0a7b3755ab82f48ba97e7b728440673410a7
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:17:38 +0200

    Refactor IIFE for better readability

--- zadanie8_liev.js
+++ zadanie8_liev.js
@@ -1,3 +1,8 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ document.getElementById('output_8').innerHTML='<button onclick="show_confirm()">Pokaż confirm box</button>'; window.show_confirm=function(){var r=confirm('Naciśnij klawisz'); if(r==true) document.getElementById('output_8').innerHTML='Nacisnąłeś OK!'; else document.getElementById('output_8').innerHTML='Nacisnąłeś Cancel!';}; })();
+(function(){ 
+document.getElementById('output_8').innerHTML='<button onclick="show_confirm()">Pokaż confirm box</button>'; 
+window.show_confirm=function(){var r=confirm('Naciśnij klawisz'); if(r==true)
+document.getElementById('output_8').innerHTML='Nacisnąłeś OK!'; 
+else document.getElementById('output_8').innerHTML='Nacisnąłeś Cancel!';}; 
+})();
