a91c22f7bd0cf395e2fbdba91ac691a8d6d698af
4.5.0 <klevicev42@gmail.com>
2025-10-22 23:06:43 +0200

    Refactor IIFE for better readability

--- zadanie15_liev.js
+++ zadanie15_liev.js
@@ -1,3 +1,7 @@
 // Autor: Kyrylo Lievichev, Nr 14
 
-(function(){ var m=parseInt(prompt('m=','3')); var denom=2*m; document.getElementById('output_15').innerHTML = '1/'+m+' = 1/'+denom+' + 1/'+denom; })();
+(function(){ 
+var m=parseInt(prompt('m=','3'));
+var denom=2*m; 
+document.getElementById('output_15').innerHTML = '1/'+m+' = 1/'+denom+' + 1/'+denom; 
+})();
